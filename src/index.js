a983970b0fe3c1c23f28ecd6eff3d34c657fa991
“coteb-ops” <Lionel-abiola.togbe@coda-student.school>
2026-05-05 14:18:19 +0200

    feat: add uptime endpoint

--- src/index.js
+++ src/index.js
@@ -114,7 +114,13 @@ const server = http.createServer((req, res) => {
       return;
     }
 
-    if (path === "/") {
+    if (path === "/") js
+    if (path === "/uptime") {
+      writeJson(res, 200, {
+        uptime: process.uptime()
+      });
+      return;
+    }  {
       writeJson(res, 200, { status: "ok", result: add(2, 3) });
       return;
     }
